b20200934b621df4dcd5800bf061b862cce6a1a8
Vasiliy Basov <vasiliy.basov.82@gmail.com>
2022-11-18 22:46:14 +0300

    puma  service

--- Files/deploy_reddit.sh
+++ Files/deploy_reddit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 runuser -l baggurd -c 'git clone -b monolith https://github.com/express42/reddit.git'
 runuser -l baggurd -c 'cd reddit && bundle install'
-sudo systemctl enable puma
-sudo systemctl start puma
+systemctl enable puma
+systemctl start puma
